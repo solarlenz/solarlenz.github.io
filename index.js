bfaf1e9fab9d2085b4ee0db200686ded2e5b9f29
solarlenz <119701167+solarlenz@users.noreply.github.com>
2023-02-16 01:25:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,23 @@
-window.onload = function() {
-  var loadingDiv = document.getElementById('loading');
-  var spinnerImg = loadingDiv.querySelector('img');
-  var pageBody = document.querySelector('body');
+//////////////////////////////////////////////
+// Spinner
+//////////////////////////////////////////////
 
-  // Set the initial background color
-  pageBody.style.backgroundColor = '#000';
+window.addEventListener('load', function() {
+  const spinner = document.createElement('div');
+  spinner.classList.add('spinner');
+  spinner.innerHTML = '<div></div><div></div><div></div><div></div>';
+  document.body.appendChild(spinner);
 
-  // Hide the loading animation when the page is fully loaded
-  loadingDiv.style.display = 'none';
-
-  // Fade in the page content and change the background color
-  spinnerImg.onload = function() {
-    loadingDiv.style.opacity = '1';
-    setTimeout(() => {
-      loadingDiv.style.display = 'none';
-      pageBody.style.backgroundColor = '#f48c06';
-      pageBody.style.transition = 'background-color 1s ease-in-out';
-      pageBody.style.opacity = '1';
-      loadingDiv.style.opacity = '0';
-    }, 1000);
-  };
-};
+  setTimeout(function() {
+    spinner.remove();
+  }, 1000);
+});
 
 
 //////////////////////////////////////////////
 // Stars
 //////////////////////////////////////////////
+
 const header = document.querySelector('header');
 const dotCount = 20;
 
