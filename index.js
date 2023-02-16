05e1098489cfe06e4b4eecb6f11ec9d47f2d463c
solarlenz <119701167+solarlenz@users.noreply.github.com>
2023-02-16 00:56:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 window.onload = function() {
-        // Hide the loading animation when the page is fully loaded
-        document.getElementById('loading').style.display = 'none';
+  var loadingDiv = document.getElementById('loading');
+  var spinnerImg = loadingDiv.querySelector('img');
+  var pageBody = document.querySelector('body');
+
+  // Set the initial background color
+  pageBody.style.backgroundColor = '#000';
+
+  // Hide the loading animation when the page is fully loaded
+  loadingDiv.style.display = 'none';
+
+  // Fade in the page content and change the background color
+  spinnerImg.onload = function() {
+    loadingDiv.style.opacity = '0';
+    setTimeout(function() {
+      loadingDiv.style.display = 'none';
+      pageBody.style.backgroundColor = '#f48c06';
+      pageBody.style.transition = 'background-color 0.5s ease-in-out';
+      pageBody.style.opacity = '1';
+    }, 500);
+  };
 };
